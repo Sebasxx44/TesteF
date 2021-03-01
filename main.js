47e16b760c61791e65a0eda277d7bf248c9e0adf
Sebasxx44 <78497558+Sebasxx44@users.noreply.github.com>
2021-03-01 15:20:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-boton').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    });
+
+    ScrollReveal().reveal('.showcase');
+    ScrollReveal().reveal('.news-cards', { delay: 300});
+    ScrollReveal().reveal('.news-baner', { delay: 300});
+    ScrollReveal().reveal('.news-baner-two', { delay: 300});
